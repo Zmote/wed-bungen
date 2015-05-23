4641ff9049e57a36c7c30d98d1f99e46e7bc3846
Zmote <shodaime.hikarikage@hotmail.com>
2015-05-23 10:35:02 +0200

    myapp2 second commit, final version

--- WED1-Ub-W6/U6.1-Node-Grundlagen/myapp2.js
+++ WED1-Ub-W6/U6.1-Node-Grundlagen/myapp2.js
@@ -2,10 +2,11 @@
  * Created by Zmote on 23.05.2015.
  */
 var http = require('http');
+var url = require('url');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-Type' : "text/plain"});
-    res.send("Hello World");
+    res.end("You requested: " + req.url);
 }).listen(3000);
 
 console.log('Listening on port 3000');
